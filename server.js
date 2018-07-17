19e4d04c80fef7995c30b23349b3b46aab4b2669
Zuodion <zuodion@gmail.com>
2018-07-17 12:53:30 +0300

    Setup heroku port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-let app = express();
+const port = process.env.PORT || 3000;
+    let app = express();
 
 app.set('view engine', 'hbs'); //шаблонизация html страниц
 app.use((req, res, next) => {
@@ -51,6 +52,6 @@ app.get('/bad', (req, res) => {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
